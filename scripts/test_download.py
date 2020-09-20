e2f35c74f6c2a34e1f9b6eca58a64e07b4fc9378
Congyue Cui <ccui@princeton.edu>
2020-09-20 16:50:13 -0400

    update

--- scripts/test_download.py
+++ scripts/test_download.py
@@ -3,7 +3,11 @@ import obspy
 from download_util import download_event
 
 
-def load_test_params():
+if __name__ == "__main__":
+    eventname = "C201105192015A"
+    eventfile = os.path.join("../CMT/CMT.190", eventname)
+    event = obspy.read_events(eventfile)[0]
+
     params = {
         "starttime_offset": -600,
         "endtime_offset": 11000,
@@ -11,41 +15,11 @@ def load_test_params():
         "channels": None,
         "location_priorities": ["", "00", "10"],
         "channel_priorities": ["BH[ZNE12]", "HH[ZNE12]"],
-        #"channel_priorities": ["BH[ZNE12]"],
         "providers": None
     }
-    return params
 
-
-def load_prod_params():
-    params = {
-        "starttime_offset": -600,
-        "endtime_offset": 11000,
-        "networks": None,
-        "channels": None,
-        "location_priorities": ["", "00", "10"],
-        "channel_priorities": ["BH[ZNE12]", "HH[ZNE12]"],
-        #"channel_priorities": ["BH[ZNE12]"],
-        "providers": None
-    }
-    return params
-
-
-def main():
-    eventname = "C201801210106A"
-    eventfile = os.path.join("../CMT/CMT.360", eventname)
-    event = obspy.read_events(eventfile)[0]
-
-    #params = load_test_params()
-    params = load_prod_params()
-
-    #basedir = "./test_data_repo"
-    basedir = "/gpfs/alpine/geo111/proj-shared/wenjie/data/raw_obsd/"
+    basedir = "./test_data_repo"
     waveform_base = os.path.join(basedir, "waveform")
     station_base = os.path.join(basedir, "station")
 
     download_event(eventname, event, params, waveform_base, station_base)
-
-
-if __name__ == "__main__":
-    main()
