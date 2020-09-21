341b8ebd09fd31f01a6a17222dd60c4b4f609940
Congyue Cui <ccui@princeton.edu>
2020-09-21 16:26:13 -0400

    update

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 from pytomo3d.signal import process_stream
 from pypers.io import process
+from proc import proc
 
 
 event = 'C201105192015A'
@@ -7,8 +8,5 @@ syn = 'raw_syn/' + event + '.raw_syn.h5'
 obs = 'raw_obs/' + event + '.raw_obs.h5'
 dst = 'proc.h5'
 
-
-def proc(syn, obs):
-    print(syn, obs)
 
 process((syn, obs), dst, proc, 'stream').run()
