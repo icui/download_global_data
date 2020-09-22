a4842562ca4f9f151c275450766e666de28d674f
Congyue Cui <ccui@princeton.edu>
2020-09-22 09:33:41 -0400

    update

--- compare.py
+++ compare.py
@@ -5,6 +5,7 @@ from pypers.utils import process, AuxiliaryData
 from obspy import read_inventory, Stream
 from scipy.fftpack import fft, fftfreq
 from pyasdf import ASDFDataSet
+from sys import argv
 import numpy as np
 import cmath
 
@@ -17,6 +18,8 @@ dt = 0.2
 freq = fftfreq(nt, dt)
 fwin = np.squeeze(np.where((freq > 1/100) & (freq < 1/40)))
 phase = np.vectorize(cmath.phase)
+ws = Space()
+mode = argv[1]
 
 
 obs_flags = {
@@ -48,7 +51,11 @@ syn_flags = {
 def process_pair(event, syn, obs):
     sta = syn[0].stats.network + '.' + syn[0].stats.station
     inv = read_inventory('eu_data_repo/station/' + event + '/' + sta + '.xml')
-    syn = process_stream(syn, inventory=inv, **syn_flags)
+    # syn = process_stream(syn, inventory=inv, **syn_flags)
+
+    if mode == 'syn':
+        return process_stream(syn, inventory=inv, **syn_flags)
+
     chosen = []
 
     try:
@@ -64,29 +71,33 @@ def process_pair(event, syn, obs):
             if tr.stats.npts > nt:
                 tr.data = tr.data[:nt]
 
-        obs = process_stream(obs, inv, **obs_flags)
+        # obs = process_stream(obs, inv, **obs_flags)
+        return process_stream(obs, inv, **obs_flags)
 
-        for cmp in ('R', 'T', 'Z'):
-            synt = syn.select(component=cmp)[0]
-            obst = obs.select(component=cmp)[0]
+        # for cmp in ('R', 'T', 'Z'):
+        #     synt = syn.select(component=cmp)[0]
+        #     obst = obs.select(component=cmp)[0]
 
-            ft_syn = fft(synt.data)[fwin]
-            ft_obs = fft(obst.data)[fwin]
+        #     ft_syn = fft(synt.data)[fwin]
+        #     ft_obs = fft(obst.data)[fwin]
 
-            if np.std(phase(ft_obs / ft_syn)) < 0.4:
-                chosen.append(cmp)
+        #     if np.std(phase(ft_obs / ft_syn)) < 0.4:
+        #         chosen.append(cmp)
 
     except:
         pass
 
-    if len(chosen):
-        return AuxiliaryData(parameters={'components': chosen})
+    # if len(chosen):
+    #     return AuxiliaryData(parameters={'components': chosen})
 
 
 def process_event(event):
     src_syn = 'raw_syn/' + event + '.raw_syn.h5'
     src_obs = 'raw_obs/' + event + '.raw_obs.h5'
-    dst = 'std/' + event + '.std.h5'
+    dst = 'proc_/' + mode + event + '.proc_' + mode + '.h5'
+    
+    if ws.has(dst):
+        return
 
     with open('CMT/CMT.190/' + event, 'r') as f:
         lines = f.readlines()
@@ -101,18 +112,26 @@ def process_event(event):
     process((src_syn, src_obs), dst, partial(process_pair, event), 'stream', output_tag='selected')
     MPI.COMM_WORLD.Barrier()
     
-    if rank == 0:
-        with ASDFDataSet(dst, mode='r', mpi=False) as ds:
-            nselected = len(ds.auxiliary_data.selected.list())
+    # if rank == 0:
+    #     with ASDFDataSet(dst, mode='r', mpi=False) as ds:
+    #         nselected = len(ds.auxiliary_data.selected.list())
 
-            if nselected > 10:
-                with open('selected.txt', 'a') as f:
-                    f.write(f'{event} {nselected}\n')
+    #         if nselected > 10:
+    #             with open('selected.txt', 'a') as f:
+    #                 f.write(f'{event} {nselected}\n')
 
 
-for event in Space('CMT/CMT.190').ls():
+start = time()
+
+for event in ws.ls('CMT/CMT.190'):
+    if time() - start > 110 * 60:
+        print('timeout')
+        break
+
     try:
         process_event(event)
     
     except Exception as e:
         print(e)
+    
+    break
