df6adf3e67ba50b4ca5bab7c10773d741acea8a3
Congyue Cui <ccui@princeton.edu>
2020-09-22 14:16:25 -0400

    update

--- test_download.py
+++ test_download.py
@@ -35,9 +35,9 @@ if __name__ == "__main__":
 
     for event in ws.ls('CMT/CMT.190'):
         if not ws.has('eu_globe/raw_obs/' + event + '.raw_obs.h5'):
-            download_convert(event)
-            # try:
+            try:
+                download_convert(event)
             
-            # except:
-            #     with open('failed.txt', 'a') as f:
-            #         f.write(event + '\n')
+            except:
+                with open('failed.txt', 'a') as f:
+                    f.write(event + '\n')
